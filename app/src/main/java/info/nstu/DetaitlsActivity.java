948f110287d9523277fc519a1883d24b28e1630e
nazmul56 <nazmul.cste07@gmail.com>
2016-04-28 02:34:07 +0600

    HTML Inside JSON

--- app/src/main/java/info/nstu/DetaitlsActivity.java
+++ app/src/main/java/info/nstu/DetaitlsActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,7 +23,13 @@ public class DetaitlsActivity extends AppCompatActivity {
 
         TextView URLtv = (TextView)findViewById(R.id.urltv);
         String UrlFrom_MainActivity= getIntent().getStringExtra("url");
-        URLtv.setText(UrlFrom_MainActivity);
+       // URLtv.setText(UrlFrom_MainActivity);
+
+        URLtv.setText(Html.fromHtml( UrlFrom_MainActivity));
+
+        // Making url clickable
+        URLtv.setMovementMethod(LinkMovementMethod.getInstance());
+        URLtv.setVisibility(View.VISIBLE);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/info/nstu/MainActivity.java
+++ app/src/main/java/info/nstu/MainActivity.java
@@ -97,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
 
+
         prepareMovieData();
 
     }
@@ -165,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 JSONObject json = new JSONObject(result);
 
+
                 String str = "";
 
                 // str=json.toString();
@@ -197,57 +199,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void prepareMovieData() {
 
-        new HttpAsyncTask().execute("http://hmkcode.appspot.com/rest/controller/get.json");
+        new HttpAsyncTask().execute("http://nazmul56.github.io/get.json");
 
    /*     Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
         movieList.add(movie);
 
-        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Shaun the Sheep", "Animation", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
-        movieList.add(movie);
-
-        movie = new Movie("Up", "Animation", "2009");
-        movieList.add(movie);
-
-        movie = new Movie("Star Trek", "Science Fiction", "2009");
-        movieList.add(movie);
-
-        movie = new Movie("The LEGO Movie", "Animation", "2014");
-        movieList.add(movie);
-
-        movie = new Movie("Iron Man", "Action & Adventure", "2008");
-        movieList.add(movie);
-
-        movie = new Movie("Aliens", "Science Fiction", "1986");
-        movieList.add(movie);
-
-        movie = new Movie("Chicken Run", "Animation", "2000");
-        movieList.add(movie);
-
-        movie = new Movie("Back to the Future", "Science Fiction", "1985");
-        movieList.add(movie);
-
-        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
-        movieList.add(movie);
-
-        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
-        movieList.add(movie);
-
-        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
-        movieList.add(movie);
-
-        */
+       */
 
 
     }
